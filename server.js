9f73e8a59b3aba7f8ce532d2aad1c23cdc107a8a
malhotrah <hitanshumalhotra@gmail.com>
2022-08-15 15:08:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,5 +12,9 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.get('/nodejs', (req, res) => {
+  res.send('Hello World From nodejs');
+});
+
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
